b5ad430932af06a763fdd71476f751e38cab1421
Ariunbayar B <ariunbayar.b@gmail.com>
2019-07-02 20:10:31 +0800

    Shows profile information

--- user/views.py
+++ user/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.conf import settings
+from main.fbclient import FBClient
 
 
 @login_required
 def profile(request):
 
-    context = {}
+    fbclient = FBClient(settings.FB_APP, request.user.access_token)
+    user_info = fbclient.fetch_user_info()
+
+    context = {
+            'name': user_info.get('name'),
+            'picture': user_info.get('picture').get('data'),
+        }
 
     return render(request, 'user/profile.html', context)
