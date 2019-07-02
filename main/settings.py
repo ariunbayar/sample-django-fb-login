d5da988960ead0cc42cb108f7596e343bf795620
Ariunbayar B <ariunbayar.b@gmail.com>
2019-07-02 14:42:42 +0800

    Local settings

--- main/settings.py
+++ main/settings.py
@@ -12,21 +12,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from .local_settings import DEBUG
+from .local_settings import SECRET_KEY
+from .local_settings import ALLOWED_HOSTS
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
